37727f1687017cd62abae07453d02b5c67011a21
matthew <matthewjf@gmail.com>
2016-05-04 23:23:42 -0700

    updated various splashy refs

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_splashy_session'
+Rails.application.config.session_store :cookie_store, key: '_offlist_session'
